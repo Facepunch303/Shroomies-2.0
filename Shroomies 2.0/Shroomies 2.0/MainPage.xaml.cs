1a0827f118d85730fd201b7fc75a9fd7f27d620b
Facepunch303 <79476919+Facepunch303@users.noreply.github.com>
2022-10-18 07:16:01 -0400

    Initial commit

---
+++ Shroomies 2.0/Shroomies 2.0/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace Shroomies_2._0
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        // variables
+        int readButtonValue = 30;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            imageLoader();
+        }
+
+        public void imageLoader()
+        {
+            // add x:name for image
+            // img1.Source = "ShroomiesLogo.png";
+        }
+
+        public void BtnClicked(object sender, EventArgs args)
+        {
+            string readButtonValueString = readButtonValue.ToString();
+            Button2.Text = readButtonValueString;
+        }
+    }
+}
